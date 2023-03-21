03784c3b164fcaa7e4674b8b4abab54adad7fec0
Geoffrey Bergeret <geoffrey@gbergeret.com>
2023-03-21 09:28:44 +0000

    Initial version of the module

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
 # terraform-aws-s3-bucket
+
+This module creates an S3 bucket with versioning, lifecycles, encryption and a
+default policy.
+
+## Basic
+* Keep old versions for 30 days (`id: ExpireNonCurrentVersion`)
+* Encrypt object using AES:256
+* Prevent deleting object versions (`Sid: DenyDeleteObjectVersion`)
+
+## Usage
+
+**IMPORTANT**: We do not pin modules to versions in our examples because of the
+difficulty of keeping the versions in the documentation in sync with the latest
+released versions. We highly recommend that in your code you pin the version to
+the exact version you are using so that your infrastructure remains stable, and
+update versions in a systematic way so that they do not catch you by surprise.
+
+```hcl
+module "aws_s3_bucket" {
+  source = "git@github.com:gbergeret/terraform-aws-s3-bucket"
+
+  bucket = "assets"
+
+  tags = {
+    Environment = "Test"
+  }
+}
+```
+
+## Inputs
+
+| Name | Description | Type | Default | Required |
+|------|-------------|------|---------|--------|
+| bucket | Name of the bucket. | `string` |  | yes |
+| policy | Policy to apply on the bucket. | `string` | `{}`  | no |
+| lifecycle_configuration_rules | A list of lifecycle rules. | `list(object)` | `[]` | no |
+| tags | Map of tags to assign to the bucket. | `map(string)` | `{}` | no |
+
+## Outputs
+
+| Name | Description | Type |
+|------|-------------|------|
+| this | Bucket object. Used to access all bucket properties (e.g. `arn`) | `object` |
---
+++ main.tf
@@ -0,0 +1,102 @@
+resource "aws_s3_bucket" "this" {
+
+  bucket = var.bucket
+
+  tags = var.tags
+
+  lifecycle {
+    prevent_destroy = true
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "this" {
+  bucket = aws_s3_bucket.this.bucket
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.aws_s3_bucket_policy.json
+}
+
+data "aws_iam_policy_document" "aws_s3_bucket_policy" {
+  override_policy_documents = [var.policy]
+
+  statement {
+    sid    = "DenyDeleteObjectVersion"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:DeleteObjectVersion"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+  }
+
+  statement {
+    sid = "DenyPutObjectWithoutEncryption"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = [true]
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "this" {
+  bucket = aws_s3_bucket.this.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "this" {
+  bucket = aws_s3_bucket.this.id
+
+  dynamic "rule" {
+    for_each = local.lifecycle_configuration_rules
+
+    content {
+      id     = rule.value.id
+      status = "Enabled"
+
+      expiration {
+        expired_object_delete_marker = try(rule.value.expiration.expired_object_delete_marker, null)
+      }
+
+      abort_incomplete_multipart_upload {
+        days_after_initiation = try(rule.value.abort_incomplete_multipart_upload.days_after_initiation, null)
+      }
+
+      noncurrent_version_expiration {
+        noncurrent_days = try(rule.value.noncurrent_version_expiration.noncurrent_days, null)
+      }
+    }
+  }
+}
