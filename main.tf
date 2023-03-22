6b2fcad7892db06ea7a2fac20b462d4e921405ae
Geoffrey Bergeret <geoffrey@gbergeret.com>
2023-03-22 15:06:37 +0000

    feat: Check Bucket versioning using terratest

--- main.tf
+++ main.tf
@@ -1,5 +1,4 @@
 resource "aws_s3_bucket" "this" {
-
   bucket = var.bucket
 
   tags = var.tags
--- test/simple_bucket_test.go
+++ test/simple_bucket_test.go
@@ -2,22 +2,29 @@ package test
 
 import (
    "testing"
+   "github.com/gruntwork-io/terratest/modules/aws"
    "github.com/gruntwork-io/terratest/modules/terraform"
+   "github.com/stretchr/testify/assert"
 )
 
 func TestSimpleBucket(t *testing.T) {
    t.Parallel()
 
-    // Construct the terraform options with default retryable errors to handle the most common
-    // retryable errors in terraform testing.
     terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
         // The path to where our Terraform code is located
         TerraformDir: "../examples/simple_bucket",
     })
 
-    // At the end of the test, run `terraform destroy` to clean up any resources that were created.
+    // At the end of the test, run `terraform destroy`
     defer terraform.Destroy(t, terraformOptions)
 
-    // Run `terraform init` and `terraform apply`. Fail the test if there are any errors.
+    // Run `terraform init` and `terraform apply`.
     terraform.InitAndApply(t, terraformOptions)
+
+    // Get the bucket so we can query AWS
+    bucket := terraform.Output(t, terraformOptions, "bucket")
+
+    // Check versioning is Enabled
+    versionningStatus := aws.GetS3BucketVersioning(t, "eu-west-1", bucket)
+    assert.Equal(t, "Enabled", versionningStatus)
 }
