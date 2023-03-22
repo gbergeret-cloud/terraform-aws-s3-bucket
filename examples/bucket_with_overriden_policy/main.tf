f6f3a68566c8685d2b97b5ea239940cb85daf091
Geoffrey Bergeret <geoffrey@gbergeret.com>
2023-03-22 16:53:35 +0000

    feat: Add a second example with bucket policy overriden

---
+++ examples/bucket_with_overriden_policy/main.tf
@@ -0,0 +1,62 @@
+resource "random_id" "bucket_suffix" {
+  byte_length = 8
+}
+
+module "aws_s3_bucket" {
+  source = "../.."
+
+  bucket = "simple-bucket-${random_id.bucket_suffix.hex}"
+
+  policy = data.aws_iam_policy_document.this.json
+}
+
+data "aws_iam_policy_document" "this" {
+  statement {
+    sid    = "RandomNewPolicy"
+    effect = "Deny"
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:DeleteObject"]
+    resources = ["${module.aws_s3_bucket.this.arn}/*"]
+  }
+
+  statement {
+    sid    = "DenyDeleteObjectVersion" # Attempt to override module policy
+    effect = "Allow"                   # <-- Allow should be discarded
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    actions   = ["s3:DeleteObjectVersion"]
+    resources = ["${module.aws_s3_bucket.this.arn}/*"]
+  }
+
+  statement {
+    sid = "DenyPutObjectWithoutEncryption" # Attempt to override module policy
+
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+
+    effect    = "Deny"
+    actions   = ["s3:PutObject"]
+    resources = ["${module.aws_s3_bucket.this.arn}/*"]
+
+    condition {
+      test     = "Null"
+      variable = "s3:x-amz-server-side-encryption"
+      values   = [false] # <-- false should be discarded
+    }
+  }
+}
+
+output "bucket" {
+  value = module.aws_s3_bucket.this.bucket
+}
