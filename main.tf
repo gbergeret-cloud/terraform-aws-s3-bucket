635580f7f99acc8664b3d1bdc66d48cf3acc014e
Geoffrey Bergeret <geoffrey@gbergeret.com>
2023-04-21 17:30:09 +0100

    feat: Enable MFA Delete protection

--- main.tf
+++ main.tf
@@ -58,6 +58,7 @@ data "aws_iam_policy_document" "aws_s3_bucket_policy" {
 resource "aws_s3_bucket_versioning" "this" {
   bucket = aws_s3_bucket.this.id
   versioning_configuration {
-    status = "Enabled"
+    status     = "Enabled"
+    mfa_delete = "Enabled"
   }
 }
